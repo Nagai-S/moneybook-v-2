718cc6f3712ca9f05f8520e235933efcbd5074c8
Nagai-S <63888620+Nagai-S@users.noreply.github.com>
2021-03-01 17:25:49 +0900

    バグ修正

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,5 +1,6 @@
 class Api::ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  protect_from_forgery with: :null_session
 
   def currentUser
     uid=request.headers[:uid]
