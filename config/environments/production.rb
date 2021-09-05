92dfa9f3fd200043f7b2229c571145c2f5992722
Nagai-S <63888620+Nagai-S@users.noreply.github.com>
2021-09-05 10:26:53 +0900

    production.rbを修正
    production.rbのメールの部分をaws用に修正した。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = {  :host => 'https://moneybook-moneybook.herokuapp.com' }
+  # for heroku
+  # config.action_mailer.default_url_options = {  :host => 'https://moneybook-moneybook.herokuapp.com' }
+  config.action_mailer.default_url_options = {  :host => 'https://japan-moneybook.com' }
   #送信方法を指定（この他に:sendmail/:file/:testなどがあります)
   config.action_mailer.delivery_method = :smtp
   #送信方法として:smtpを指定した場合は、このconfigを使って送信詳細の設定を行います
@@ -75,9 +77,9 @@ Rails.application.configure do
     address:"smtp.gmail.com",
     domain: 'gmail.com',
     port:587,
-    #gmailのユーザアカウント（xxxx@gmail.com)※念のため、credentials.yml.enc行き
+    #gmailのユーザアカウント（xxxx@gmail.com)※念のため、.env行き
     user_name: ENV['GMAIL'],
-    #gmail２段階認証回避のためにアプリケーションでの利用パスワードを取得、必ずcredentials.yml.endに設定を！！
+    #gmail２段階認証回避のためにアプリケーションでの利用パスワードを取得、必ず.envに設定を！！
     password: ENV['GMAIL_PASSWORD'],
     #パスワードをBase64でエンコード
     authentication: :login
