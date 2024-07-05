418009580e570deb7ce0044110b1c3d54bb75bab
Nagai-S <63888620+Nagai-S@users.noreply.github.com>
2024-07-05 12:22:56 +0900

    Update migration file

--- db/migrate/20240704035844_add_currency_to_account_exchanges.rb
+++ db/migrate/20240704035844_add_currency_to_account_exchanges.rb
@@ -1,7 +1,5 @@
 class AddCurrencyToAccountExchanges < ActiveRecord::Migration[6.0]
   def change
-    remove_column :account_exchanges, :to_currency_id
-    remove_column :account_exchanges, :source_currency_id
     # add_reference :account_exchanges, :to_currency, foreign_key: {to_table: :currencies}
     # add_reference :account_exchanges, :source_currency, foreign_key: {to_table: :currencies}
     # AccountExchange.find_each do |ax|
