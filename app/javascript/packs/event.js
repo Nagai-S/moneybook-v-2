9caf037435302d0f494acf4416cb54b2740e1393
Nagai-S <63888620+Nagai-S@users.noreply.github.com>
2024-08-30 18:17:42 +0900

    Update event.js

--- app/javascript/packs/event.js
+++ app/javascript/packs/event.js
@@ -5,9 +5,21 @@ $(function () {
     $(".currency-select").val(currency_id);
   });
 
+  $(".account-select").eq(2).on("change", function () {
+    let currency_id = $(".account-select option:selected").eq(2).data("currency");
+    console.log(currency_id);
+    $(".currency-select").eq(2).val(currency_id);
+  });
+
   $(".card-select").on("change", function () {
     let currency_id = $(".card-select option:selected").data("currency");
     console.log(currency_id);
     $(".currency-select").val(currency_id);
+  });
+
+  $(".card-select").eq(2).on("change", function () {
+    let currency_id = $(".card-select option:selected").eq(2).data("currency");
+    console.log(currency_id);
+    $(".currency-select").eq(2).val(currency_id);
   });
 });
